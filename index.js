4ee50de536ef958c30c102ad5f12201acc432036
Ibrahim-Payak <114486038+Ibrahim-Payak@users.noreply.github.com>
2022-11-16 06:32:50 +0000

    first route created

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("codespace started");
+//index.js is used for listening
+
+const app = require('./app')
+
+const {PORT} = process.env
+
+app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`)
+})
